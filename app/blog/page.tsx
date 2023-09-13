5414a73cef9d938d20e38dbc6a775ead519c2712
Alexandr <alexandr.falkone@yandex.ru>
2023-09-13 17:31:05 +0300

    initial

---
+++ app/blog/page.tsx
@@ -0,0 +1,35 @@
+import { Metadata } from "next";
+import Link from "next/link";
+
+async function getData() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  if (!response.ok) throw new Error("Unable to fetch posts!");
+
+  return response.json();
+}
+
+export const metadata: Metadata = {
+  title: "Blog | Next App",
+};
+
+export default async function Blog() {
+  const posts = await getData();
+
+  return (
+    <>
+      <h1>Blog page</h1>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.id}>
+            <Link href={`/blog/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ components/TheFooter.tsx
@@ -0,0 +1,5 @@
+const TheFooter = () => {
+    return <footer className="container">Created by &copy;Aleksandr</footer>;
+  };
+  
+  export { TheFooter };
